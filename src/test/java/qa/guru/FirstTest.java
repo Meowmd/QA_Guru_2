46da2f4ab6f254f16c1b19348be20bc85a309455
Mariya Bykova <bykova.m@gmail.com>
2022-06-01 13:43:31 +0700

    Change test 2

--- src/test/java/qa/guru/FirstTest.java
+++ src/test/java/qa/guru/FirstTest.java
@@ -6,11 +6,10 @@ import org.junit.jupiter.api.Test;
 public class FirstTest {
     @Test
     void firstTest() {
-        Assertions.assertTrue(3 > 2);
+        Assertions.assertTrue(3 >= 2);
     }
-    public class FirstTest {
         @Test
         void SecondTest() {
-            Assertions.assertTrue(3 > 2);
+            Assertions.assertTrue(3 >= 2);
         }
-
+    }
